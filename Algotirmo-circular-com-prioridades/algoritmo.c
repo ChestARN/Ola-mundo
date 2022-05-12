6a61b6012875610b87206f9a4a743b7055715ef9
Pedro Henrique PHD <danesvara2002@gmail.com>
2022-05-12 19:15:44 -0300

    Algoritmo circular com prioridade
    Atividade de avaliativa de Sistemas Operacionais

---
+++ Algotirmo-circular-com-prioridades/algoritmo.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+/*
+Variáveis necessárias:
+espera
+cpu(executando)
+pronto
+processos(1, 2, 3, 4, 5){
+ devem ter:
+  tamanho
+  prioridade
+  posição na fila
+}
+*/
+typedef struct processo
+{
+ int tamanho;
+ int prioridade;
+ int posicaoNaFila;
+}P;
+
+void imprimeDados(P processo)
+{
+ printf("Tempo necessário de Processo: %i\n", processo.tamanho);
+ printf("Prioridade do Processo: %i\n", processo.prioridade);
+ if(processo.tamanho < processo.prioridade)
+  {
+    processo.prioridade = processo.tamanho;
+  }
+ while (processo.prioridade >= 1)
+ {
+   printf("Tempo de execução: %i\n", processo.prioridade);
+   processo.prioridade--;
+ }
+}
+
+int main()
+{
+ P p1 = {13, 3, 1};
+ while(p1.tamanho >= 0)
+ {
+  imprimeDados(p1);
+  p1.tamanho = p1.tamanho - p1.prioridade;
+ }
+ if(p1.tamanho < 0)
+  {
+  p1.tamanho = 0;
+  }
+ printf("%i", p1.tamanho);
+ return 0;
+}
