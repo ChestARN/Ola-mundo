83b5a9826bb25e38b39b26508b4ef357bf9b8996
Pedro Henrique PHD <danesvara2002@gmail.com>
2022-05-13 17:50:02 -0300

    Atualização 13/05/2022

--- Aulas-de-JavaScript/aula 1/aula.js
+++
@@ -1,5 +0,0 @@
-let nome = 'Rafael'; //string literal
-let idade = 30; //number literal
-let estaAprovado = true; //boolean
-let sobrenome; //undefined
-let corSelecionado = null; // redefinir um valor
---
+++ Aulas-de-JavaScript/aula youtube/aula.js
@@ -0,0 +1,8 @@
+let cor = 'vermelho';
+
+function trocaCor(novaCor){
+ cor = novaCor;
+}
+console.log(cor);
+trocaCor('azul');
+console.log(cor);
