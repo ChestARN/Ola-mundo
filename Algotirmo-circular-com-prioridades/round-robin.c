47e0494420fb5b11c4cef3a73c4d87faf27f3c8f
Pedro Henrique PHD <danesvara2002@gmail.com>
2022-05-17 19:03:01 -0300

    att 1

--- Algotirmo-circular-com-prioridades/round-robin.c
+++ Algotirmo-circular-com-prioridades/round-robin.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//int processos[2];
+int processos[2];
 int tempo[3];
 
-int main () {
-    imprimeTabela();
-    return 0;
+void preencheProcessos(){
+    printf("Defina o quantum de cada processo: \n");
+    printf("P1: ");
+    scanf("%d", &processos[0]);
+    printf("\nP2: ");
+    scanf("%d", &processos[1]);
+    printf("\n");
+    printf("Agora defina o tempo total de execução para cada processo: \n");
+    printf("P1: ");
+    scanf("%d", &tempo[0]);
+    printf("\nP2: ");
+    scanf("%d", &tempo[1]);
+    printf("\n\n\n\n");
 }
 
 void imprimeTabela () {
-    tempo[1] = 3;
-    tempo[0] = 2;
     tempo[2] = tempo[1] + tempo[0];
     printf("Tempo    P1    P2\n");
     for(int i = 0; i < tempo[2]; i++){
-        printf("%d - %d\n", i, i + 1);
+        printf("%d - %d    ##    --\n", i, i + 1);
     }
+}
+
+int main () {
+    preencheProcessos();
+    imprimeTabela();
+    return 0;
 }
