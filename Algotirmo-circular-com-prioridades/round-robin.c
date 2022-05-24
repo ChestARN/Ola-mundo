69511380d35d44b4c25897f0e404bf017708c6b7
Caio César <caiocesar132016@gmail.com>
2022-05-24 05:04:38 -0300

    ISSO É COISA DE MALUCO E ESQUIZOFRÊNICO, NUNCA MAIS QUERO MEXER COM C NA MINHA VIDA
    Se nem Deus entendeu o que era pra eu fazer, quem dirá eu mesmo. Desisti de mexer no código depois de 5 horas. Isso é coisa de louco. IMPOSSÍVEL aplicar isso em C com a falta de variável dinâmica no código normal e a INEXISTÊNCIA de um unsized array, além de um PÉSSIMO comportamento de objeto por parte do C, tentei aplicar struct, muitas funções por ai e NADA(não testei ponteiro por questões de saúde mental). Meus pêsames, apenas 2 processos e um monte de Printf e If's como prioridade e é isso o que nós temos. Obrigado por nada criador da linguagem C.

--- Algotirmo-circular-com-prioridades/round-robin.c
+++ Algotirmo-circular-com-prioridades/round-robin.c
@@ -1,53 +1,116 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <locale.h>
 
-int processos[2];
-int tempo[3];
+int processos[2], tempo[3], prioridade[2], duracao[10], tempoEspera[10];
 
-void preencheProcessos(){
-    setlocale(LC_ALL, "Portuguese");
-    printf("Defina o quantum de cada processo: \n");
-    printf("P1: ");
-    scanf("%d", &processos[0]);
-    printf("P2: ");
-    scanf("%d", &processos[1]);
-    printf("\n");
-    printf("Agora defina o tempo total de execução para cada processo: \n");
-    printf("P1: ");
-    scanf("%d", &tempo[0]);
-    printf("\nP2: ");
-    scanf("%d", &tempo[1]);
-    printf("\n\n\n\n");
-}
-
-void imprimeTabela () {
-    tempo[2] = tempo[1] + tempo[0];
-    printf("Executando no quantum : ##\n");
-    printf("Não executando no quantum: --\n\n");
-    printf("Tempo    P1    P2\n");
-    for(int i = 0; i < tempo[2]; i++){
-            for(int j = processos[0]; j > 0; j--){
-                if(tempo[0] > 0){
-                    printf("%d - %d    ##    --\n", i, i + 1);
-                    i++;
-                    tempo[0]--;
-                }
-            }
-            for(int j = processos[1]; j > 0; j--){
-                if(tempo[1] > 0){
-                    printf("%d - %d    --    ##\n", i, i + 1);
-                    i++;
-                    tempo[1]--;
-                } else {
-                    i--;
-                }
-            }
-    }
-}
+void preencheProcessos();
+void imprimeTabela();
 
 int main () {
     preencheProcessos();
     imprimeTabela();
     return 0;
+}
+
+void preencheProcessos(){
+    setlocale(LC_ALL, "Portuguese");
+    printf("\n");
+    
+    //Escolher o quantum do programa
+    printf("Defina o quantum(Intervalo de tempo) entre os processamentos: \n");
+    printf("Processo 1: ");
+    scanf("%d" ,&processos[0]);
+    printf("\n");
+    printf("Processo 2: ");
+    scanf("%d", &processos[1]);
+    printf("\n"); 
+	
+	//Define o tempo de execução de cada processo
+	printf("Defina o tempo de execução para os processos: \n");
+	printf("Processo 1: ");
+	scanf("%d", &tempo[0]);
+	printf("\n");
+	printf("Processo 2: ");
+	scanf("%d", &tempo[1]);
+	
+	//Define a prioridade de cada processo
+	printf("Defina a prioridade de cada processo: \n");
+	printf("Processo 1: ");
+	scanf("%d", &prioridade[0]);
+	printf("\n");
+	printf("Processo 2: ");
+	scanf("%d", &prioridade[1]);
+	printf("\n");
+}
+void imprimeTabela(){
+	
+	tempo[2] = tempo[1] + tempo[0];
+    
+    if(prioridade[0] < prioridade[1]){
+    	
+    	printf("O processo 1 tem prioridade!\n");
+    	printf("Duração do processo 1: %d \n", tempo[0]);
+		printf("Duração do processo 2: %d \n", tempo[1]);
+		printf("Tempo de chegada do processo 1: Já está sendo executado! \n");
+		printf("Tempo de chegada do processo 2: %d \n", tempo[0]);
+		printf("\n");
+		printf("Quantum sendo executado:  ##\n");
+    	printf("Quantum não executado: --\n\n");
+		printf("Tempo    P1    P2\n");
+	    for(int i = 0; i < tempo[2]; i++){
+	    	
+	            for(int k = processos[0]; k > 0; k--){
+	            	
+	                if(tempo[0] > 0){
+	                    printf("%d - %d    ##    --\n", i, i + 1);
+	                    i++;
+	                    tempo[0]--;
+	                }
+	            }
+	            for(int k = processos[1]; k > 0; k--){
+	            	
+	                if(tempo[1] > 0){
+	                    printf("%d - %d    --    ##\n", i, i + 1);
+	                    i++;
+	                    tempo[1]--;
+	                    
+	                } else {
+	                	
+	                    i--;
+	                }
+	            }
+	    }
+	}else{
+		printf("O processo 2 tem prioridade!\n");
+		printf("Duração do processo 1: %d \n", tempo[0]);
+		printf("Duração do processo 2: %d \n", tempo[1]);
+		printf("Tempo de chegada do processo 1: %d \n", tempo[1]);
+		printf("Tempo de chegada do processo 2: Já está sendo executado!\n");
+		printf("\n");
+		printf("Quantum sendo executado:  ##\n");
+    	printf("Quantum não executado: --\n\n");
+		printf("Tempo    	P1    P2\n");
+		for(int i = 0; i < tempo[2]; i++){
+			
+	            for(int k = processos[0]; k > 0; k--){
+	                if(tempo[0] > 0){
+	                    printf("%d - %d    	--    ##\n", i, i + 1);
+	                    i++;
+	                    tempo[0]--;
+	                }
+	            }
+	            for(int k = processos[1]; k > 0; k--){
+	            	
+	                if(tempo[1] > 0){
+	                    printf("%d - %d    	##    --\n", i, i + 1);
+	                    i++;
+	                    tempo[1]--;
+	                    
+	                } else {
+	                    i--;
+	                }
+	            }
+	    }
+	    
+	}
 }
